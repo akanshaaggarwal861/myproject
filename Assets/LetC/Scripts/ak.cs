63c8c7cc578b87e7d1bb0e099a4ea1aa3c238480
lenvovo-PC\lenvovo <shivambansal@metadesignsolutions.co.uk>
2019-09-02 13:16:44 +0530

    2 commit

---
+++ Assets/LetC/Scripts/ak.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ak : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("my new project");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
